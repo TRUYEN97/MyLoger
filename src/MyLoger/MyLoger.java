315ea05c8aa381b6a4996631a4b1c9f682172edd
SQT <samtruyen60@mail.com>
2022-05-19 10:27:48 +0700

    add to queue log

--- src/MyLoger/MyLoger.java
+++ src/MyLoger/MyLoger.java
@@ -9,9 +9,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -22,9 +24,14 @@ public class MyLoger {
     private FileWriter writer;
     private File file;
     private final TimeBase timeBase;
+    private final List<Queue<String>> queueLogs;
+    private final Queue<String> queuelog;
+    private boolean isOpen;
 
     public MyLoger() {
         this.timeBase = new TimeBase();
+        this.queueLogs = new ArrayList<>();
+        this.queuelog = new ArrayDeque<>();
     }
 
     public boolean begin(File file, boolean append) {
@@ -34,7 +41,7 @@ public class MyLoger {
             }
             this.writer = new FileWriter(file, append);
             this.file = file;
-            return true;
+            return isOpen = true;
         } catch (IOException ex) {
             System.err.println(ex);
             return false;
@@ -50,6 +57,13 @@ public class MyLoger {
         return begin(file, append);
     }
 
+    public Queue<String> getQueueLog() {
+        if (this.queueLogs.isEmpty()) {
+            return addQueueToList(this.queuelog);
+        }
+        return addQueueToList(new ArrayDeque<>(this.queuelog));
+    }
+
     public void addLog(Object txt) {
         if (txt == null) {
             add("null");
@@ -60,29 +74,41 @@ public class MyLoger {
         }
     }
 
-    public Scanner getScaner() {
-        try {
-            return new Scanner(this.file);
-        } catch (FileNotFoundException ex) {
-            return null;
-        }
-    }
-
     private void add(String data) {
+        if (!isOpen) {
+            System.err.println("Loger has close!");
+            return;
+        }
         try {
             String log = String.format("%s : %s\r\n",
                     this.timeBase.getDateTime(TimeBase.UTC,
                             TimeBase.SIMPLE_DATE_TIME), data.trim());
+            addToQueue(log);
             this.writer.write(log);
             this.writer.flush();
         } catch (IOException ex) {
             System.err.println(ex);
+            isOpen = false;
         }
+    }
+
+    private void addToQueue(String log) {
+        if (queueLogs.isEmpty()) {
+            this.queuelog.add(log);
+        }
+        for (Queue<String> queueLog : queueLogs) {
+            queueLog.add(log);
+        }
+    }
+
+    private Queue<String> addQueueToList(Queue<String> queue) {
+        this.queueLogs.add(queue);
+        return this.queuelog;
     }
 
     public String getLog() {
         StringBuilder builder = new StringBuilder();
-        try (Scanner scanner = new Scanner(this.file)) {
+        try ( Scanner scanner = new Scanner(this.file)) {
             while (scanner.hasNextLine()) {
                 builder.append(scanner.nextLine()).append("\r\n");
             }
@@ -94,9 +120,11 @@ public class MyLoger {
     }
 
     public void close() {
+        this.queueLogs.clear();
         if (this.writer != null) {
             try {
                 this.writer.close();
+                isOpen = false;
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
