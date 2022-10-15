7d78cbf148cb2c4e09098da9c80c52cadfa8abbd
SQT <samtruyen60@mail.com>
2022-10-15 11:22:35 +0700

    create contruster set default time zome

--- src/MyLoger/MyLoger.java
+++ src/MyLoger/MyLoger.java
@@ -14,6 +14,7 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.TimeZone;
 
 /**
  *
@@ -27,7 +28,12 @@ public class MyLoger implements Cloneable {
     private final List<Queue<String>> queueLogs;
 
     public MyLoger() {
-        this.timeBase = new TimeBase();
+        this.timeBase = new TimeBase(TimeBase.UTC);
+        this.queueLogs = new ArrayList<>();
+    }
+    
+    public MyLoger(TimeZone timeZone) {
+        this.timeBase = new TimeBase(timeZone);
         this.queueLogs = new ArrayList<>();
     }
 
@@ -61,28 +67,24 @@ public class MyLoger implements Cloneable {
     public synchronized void addLog(Object txt) throws IOException {
         if (txt == null) {
             add(String.format("%s:  null\r\n",
-                    this.timeBase.getDateTime(TimeBase.UTC,
-                            TimeBase.DATE_TIME_MS)));
+                    this.timeBase.getDateTime(TimeBase.DATE_TIME_MS)));
             return;
         }
         for (String line : txt.toString().split("\n")) {
             add(String.format("%s:   %s\r\n",
-                    this.timeBase.getDateTime(TimeBase.UTC,
-                            TimeBase.DATE_TIME_MS), line.trim()));
+                    this.timeBase.getDateTime(TimeBase.DATE_TIME_MS), line.trim()));
         }
     }
 
     public synchronized void addLog(String key, Object str) throws IOException {
         if (str == null) {
             add(String.format("%s:  [%s] null\r\n",
-                    this.timeBase.getDateTime(TimeBase.UTC,
-                            TimeBase.DATE_TIME_MS), key));
+                    this.timeBase.getDateTime(  TimeBase.DATE_TIME_MS), key));
             return;
         }
         for (String line : str.toString().split("\n")) {
             add(String.format("%s:   [%s] %s\r\n",
-                    this.timeBase.getDateTime(TimeBase.UTC,
-                            TimeBase.DATE_TIME_MS), key, line.trim()));
+                    this.timeBase.getDateTime(TimeBase.DATE_TIME_MS), key, line.trim()));
         }
     }
 
